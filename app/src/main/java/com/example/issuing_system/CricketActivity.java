1ea4328aa11a01e8138a9cd7ae6908c835b11cf4
hetpandya29 <hetpandya290102@gmail.com>
2023-03-29 10:55:58 +0530

    8th commit

--- app/src/main/java/com/example/issuing_system/CricketActivity.java
+++ app/src/main/java/com/example/issuing_system/CricketActivity.java
@@ -1,12 +1,14 @@
 package com.example.issuing_system;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -18,6 +20,8 @@ public class CricketActivity extends AppCompatActivity {
     Spinner sp;
 
     TextView value,sname;
+
+    Button confirm;
     int count = 0;
 
 
@@ -31,6 +35,7 @@ public class CricketActivity extends AppCompatActivity {
         backb = findViewById(R.id.back_arrow);
         sname = findViewById(R.id.sname);
         sp = findViewById(R.id.spinner);
+        confirm = findViewById(R.id.button1);
 
         backb.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,7 +48,19 @@ public class CricketActivity extends AppCompatActivity {
         ArrayAdapter ar = ArrayAdapter.createFromResource(this,R.array.cricket, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
         sp.setAdapter(ar);
 
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(CricketActivity.this,Issued_Receipt.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
+
+
 
     public void increment (View v){
         if(count >= 3 ) count = 3;
--- app/src/main/java/com/example/issuing_system/TableTenisActivity.java
+++ app/src/main/java/com/example/issuing_system/TableTenisActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -18,6 +19,8 @@ public class TableTenisActivity extends AppCompatActivity {
     Spinner sp;
 
     TextView value,sname;
+
+    Button confirm;
     int count = 0;
 
 
@@ -31,6 +34,7 @@ public class TableTenisActivity extends AppCompatActivity {
         backb = findViewById(R.id.back_arrow);
         sname = findViewById(R.id.sname);
         sp = findViewById(R.id.spinner);
+        confirm = findViewById(R.id.button1);
 
         backb.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +46,16 @@ public class TableTenisActivity extends AppCompatActivity {
 
         ArrayAdapter ar = ArrayAdapter.createFromResource(this,R.array.table_tennis, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
         sp.setAdapter(ar);
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(TableTenisActivity.this,Issued_Receipt.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
 
